1165a039cff5c16971b3d16e38453a147d520d5d
noxiouse <quastgames@gmail.com>
2020-12-10 02:25:15 +0100

    facing the ore destoreying now owrks

--- src/main/java/com/noxiouse/creativeminer/events/TestJumpEvent.java
+++ src/main/java/com/noxiouse/creativeminer/events/TestJumpEvent.java
@@ -5,8 +5,12 @@ import com.noxiouse.creativeminer.init.BlockInit;
 import com.sun.javafx.geom.Vec3d;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.OreBlock;
 import net.minecraft.dispenser.IPosition;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
@@ -14,6 +18,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -44,16 +49,17 @@ public class TestJumpEvent {
     public static void detectOre(LivingEvent.LivingUpdateEvent event){
         LivingEntity livingEvent = event.getEntityLiving();
         World world = livingEvent.getEntityWorld();
-        //Vector3d start = new Vector3d(livingEvent.getPosX(), livingEvent.getPosYEye(), livingEvent.getPosZ());
         Vector3d start = livingEvent.getEntity().getPositionVec().add(0,livingEvent.getEyeHeight(),0);
         Vector3d end = new Vector3d(livingEvent.getEntity().getLookVec().getX(),livingEvent.getEntity().getLookVec().getY(),livingEvent.getEntity().getLookVec().getZ());
         BlockState detect = world.getBlockState(world.rayTraceBlocks(new RayTraceContext(start,start.add(end), RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.NONE,livingEvent.getEntity())).getPos());
 
-        if(detect.getBlock().matchesBlock(Blocks.IRON_ORE)){
-            world.destroyBlock(world.rayTraceBlocks(new RayTraceContext(start,start.add(end), RayTraceContext.BlockMode.VISUAL, RayTraceContext.FluidMode.NONE,livingEvent.getEntity())).getPos(),true);
-            CreativeMiner.LOGGER.info("iron" + detect);
-        } else {
-            CreativeMiner.LOGGER.info("livingupdaeevent");
-        }
+        Chunk chunk = world.getChunkAt(livingEvent.getEntity().getPosition());
+
+        BlockState chunks = chunk.getBlockState(world.rayTraceBlocks(new RayTraceContext(start, start.add(end), RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.NONE, livingEvent.getEntity())).getPos());
+
+            if(chunks.getBlock().matchesBlock(Blocks.IRON_ORE)){
+                CreativeMiner.LOGGER.info(detect);
+                world.destroyBlock(world.rayTraceBlocks(new RayTraceContext(start,start.add(end), RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.NONE,livingEvent.getEntity())).getPos(),true);
+            }
     }
 }
